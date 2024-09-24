69f214a19645cd36a934ada0f425e56f46c09ce6
juaniitosb <55324354+juaniitosb@users.noreply.github.com>
2024-09-23 20:11:34 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   listUser: `${github}search/users?q=YOUR_NAME`,
   listInfoUser: `${github}users/`,
   //localHost:back
-  //localHost:back2
+  //localHost:back2232
   login: `${localhost}managerTask/api/ts/login`,
   personRegister: `${localhost}managerProducts/api/person/createPerson`,
   userRegister: `${localhost}managerTask/api/ts/createUser`,
